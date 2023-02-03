0153e91e07e41c56e3436280961ac633a8ced39e
giorgi orjonikidze <gioorjo123@gmail.com>
2023-02-03 20:14:27 +0400

    clone my repository react-tailwind for easyer setup, add colors in tailwind.config.js, add images, add font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      red_error: "#E52F2F",
+      red_font: "#F93B1D",
+      green: "#98E37E",
+      grey: "#BCBCBC",
+      purple: "#6B40E3",
+      dark: "#2E2E2E"
+    },
+    
   },
   plugins: [],
 };
